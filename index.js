6356c75f1bf79e47c7e5fa3fbdb5ed3fad1b8065
sexmastercc <oscarlin@edu.schools.us.kg>
2024-06-30 20:48:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://github.com';
+const github = 'https://github.com';
 
 const proxy = createProxyMiddleware({
-  target: nggUrl,
+  target: github,
   changeOrigin: true,
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'mathsspot.com') {
-      req.headers['X-Forwarded-For'] = ''; 
-      req.headers['X-Real-IP'] = '';
-      req.headers['Via'] = '';
-    }
-    return nggUrl;
+    return github;
   }
 });
 
-// Apply the proxy middleware only to requests starting with '/ngg'
-app.use('/ngg', proxy);
+app.use('/github', proxy);
 
 const port = process.env.PORT || 443;
 app.listen(port, () => {
