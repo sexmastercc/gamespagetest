ad1ac5aa9b72fb8cd5d28965f286405fa407bcdf
sexmastercc <oscarlin@edu.schools.us.kg>
2024-06-30 20:39:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://mathsspot.com';
+const nggUrl = 'https://github.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
